86221cc96ced34a2b258b3e9657c0d2dce1f9eb4
Daniyal Khan <daniyal.khan@graymatrix.com>
2025-07-02 13:08:12 +0530

    fixing the retrieval score

--- nodes.py
+++ nodes.py
@@ -3,7 +3,7 @@ from retriever import retriever
 # Define your custom nodes
 def retrieve_answer(state):
     query = state["question"]
-    docs_with_scores = retriever.similarity_search_with_score(query, k=1)
+    docs_with_scores = retriever.similarity_search_with_score(query, k=1, fetch_k=3)
 
     docs = []
     for doc, score in docs_with_scores:
@@ -18,9 +18,9 @@ def is_confident_enough(state):
     docs = state["retrieved_docs"]
     print(f"Retrived docs:\n{docs}")
     doc = state["retrieved_docs"][0]
-    score = doc.metadata.get("score", 0.0)
+    score = doc.metadata.get("score", 100.0)
     
-    return {**state,"condition": "yes" if score > 0.75 else "no"}
+    return {**state,"condition": "yes" if score < 1 else "no"}  # lower = more similar
 
 
 def generate_answer(state):
